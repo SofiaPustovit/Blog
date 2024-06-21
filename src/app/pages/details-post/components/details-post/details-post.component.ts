63d49d1648f19da65b0251323f9f27626fa828fc
Sofia <sofiyakot550@gmail.com>
2024-06-21 15:35:38 +0300

    details page

--- src/app/pages/details-post/components/details-post/details-post.component.ts
+++ src/app/pages/details-post/components/details-post/details-post.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PostService } from '../../../../services/post.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { IPost } from '../../../../intefaces/post.interface';
 
 @Component({
   selector: 'app-details-post',
   templateUrl: './details-post.component.html',
   styleUrl: './details-post.component.scss',
 })
-export class DetailsPostComponent {}
+export class DetailsPostComponent implements OnInit {
+  posts: IPost[] = [];
+  post$!: Observable<IPost>;
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.post$ = this.route.params.pipe(
+        switchMap((params: Params) => {
+          return this.postService.getById(params['id']);
+        }),
+      );
+      this.postService.getAllPost().subscribe((responce) => {
+        this.posts = responce;
+      });
+    });
+  }
+}
--- src/app/pages/details-post/details.post.module.ts
+++ src/app/pages/details-post/details.post.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailsPostComponent } from './components/details-post/details-post.component';
 import { RouterLink, RouterModule } from '@angular/router';
+import { PostService } from '../../services/post.service';
+import { QuillViewHTMLComponent } from 'ngx-quill';
+import { PostModule } from '../../modules/post/post.module';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterLink,
     RouterModule.forChild([{ path: ':id', component: DetailsPostComponent }]),
+    QuillViewHTMLComponent,
+    PostModule,
   ],
   declarations: [DetailsPostComponent],
   exports: [DetailsPostComponent],
+  providers: [PostService],
 })
 export class DetailsPostModule {}
